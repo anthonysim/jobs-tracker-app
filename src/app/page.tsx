23a3b2a8bbb5aa7e4bd49cdd2bfe8802e9d9572f
Anthony Sim <anthonyysim@gmail.com>
2025-06-29 13:32:02 -0700

    added supabase and changed to form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,40 +14,55 @@ export default function Home() {
     ? AuthPrompt.NoAccount
     : AuthPrompt.AlreadyHaveAccount;
 
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="max-w-md mx-auto mt-10 p-6 rounded shadow bg-gray-900 text-white space-y-4 border border-gray-700">
+        <form
+          onSubmit={submitHandler}
+          className="max-w-md mx-auto mt-10 p-6 rounded shadow bg-gray-900 text-white space-y-4 border border-gray-700"
+        >
           <h1 className="text-3xl text-center">Jobs Status Tracker</h1>
           <h2 className="text-2xl font-bold text-center">{title}</h2>
 
           <input
             type="email"
             placeholder="Email"
+            // value={email}
+            // onChange={(e) => setEmail(e.target.value)}
             className="w-full bg-gray-800 border border-gray-600 p-2 mb-2 rounded text-white placeholder-gray-400"
+            required
           />
           <input
             type="password"
             placeholder="Password"
+            // value={password}
+            // onChange={(e) => setPassword(e.target.value)}
             className="w-full bg-gray-800 border border-gray-600 p-2 mb-4 rounded text-white placeholder-gray-400"
+            required
           />
 
           <button
             className={`text-white w-full px-4 py-2 rounded hover:cursor-pointer ${actionColor}`}
-            onClick={() => console.log(`${title} Clicked`)}
+            type="submit"
           >
             {title}
           </button>
 
           <div className="mt-4 text-center">
             <button
+              type="button"
               className="text-sm text-blue-400 hover:underline hover:cursor-pointer"
               onClick={() => setIsSignIn((prev) => !prev)}
             >
               {toggleText}
             </button>
           </div>
-        </div>
+        </form>
       </main>
     </div>
   );
